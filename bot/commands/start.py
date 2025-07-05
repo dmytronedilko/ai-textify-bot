812bf9e16eb499206c38b6b59041d0e46ddbbb14
Vadym <67538058+Extractoro@users.noreply.github.com>
2025-07-05 14:00:02 +0300

    Revert "Ait 13 update readme md"

--- bot/commands/start.py
+++
@@ -1,9 +0,0 @@
-from aiogram import Router, types
-from aiogram.filters import Command
-
-router = Router()
-
-
-@router.message(Command("start"))
-async def start_command(message: types.Message):
-    await message.answer("Hi! Send me a voice message to get started.")
