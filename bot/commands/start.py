2175ea69dbb8b08d2a3539362f249d838653a2e7
Extractoro <vadymtytarenkoo@gmail.com>
2025-07-05 12:14:44 +0300

    style: code styling

--- bot/commands/start.py
+++ bot/commands/start.py
@@ -3,6 +3,7 @@ from aiogram.filters import Command
 
 router = Router()
 
+
 @router.message(Command("start"))
 async def start_command(message: types.Message):
     await message.answer("Hi! Send me a voice message to get started.")
