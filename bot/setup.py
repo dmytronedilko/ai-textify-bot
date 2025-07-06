1428e9647c5b3ca10053468521d715cfbbf88884
Extractoro <vadymtytarenkoo@gmail.com>
2025-07-06 17:48:02 +0300

    feat: command /support

--- bot/setup.py
+++ bot/setup.py
@@ -3,7 +3,7 @@ from aiogram import Bot, Dispatcher
 from dotenv import load_dotenv
 from openai import OpenAI
 
-from bot.commands import start
+from bot.commands import start, support
 from bot.handlers.textify_handler import register_textify_handler
 from bot.handlers.video_note_handler import register_video_note_handler
 from bot.handlers.voice_handler import register_voice_handler
@@ -15,6 +15,7 @@ dp = Dispatcher()
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 dp.include_router(start.router)
+dp.include_router(support.router)
 dp.include_router(register_voice_handler(bot, client))
 dp.include_router(register_video_note_handler(bot, client))
 dp.include_router(register_textify_handler(bot, client))
